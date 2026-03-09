770c1bf56eeffee645d6fcc940cef429f4f3c588
Colin <Colin@DESKTOP-727BBMK.umd.edu>
2026-03-09 00:11:41 -0400

    Anonymous Class And Wall Warping

--- src/game/PlayerChar.java
+++ src/game/PlayerChar.java
@@ -3,75 +3,89 @@ package game;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
 public class PlayerChar extends Polygon implements KeyListener {
 	boolean forward = false;
 	boolean right = false;
 	boolean left = false;
 	double moveStep = 5;
-	
-	public PlayerChar(Point[] shape, Point position, double rotation)
-	{
-		super(shape,position,rotation);
-		
+
+	public PlayerChar(Point[] shape, Point position, double rotation) {
+		super(shape, position, rotation);
+
 	}
-	 public void paint(Graphics brush) {
 
-	        Point[] points = this.getPoints();
+	public void paint(Graphics brush) {
 
-	        int[] x = new int[points.length];
-	        int[] y = new int[points.length];
+		Point[] points = this.getPoints();
 
-	        for(int i = 0; i < points.length; i++) {
-	            x[i] = (int) points[i].getX();
-	            y[i] = (int) points[i].getY();
-	        }
+		int[] x = new int[points.length];
+		int[] y = new int[points.length];
 
-	        brush.drawPolygon(x, y, points.length);
-	    }
-	    public void move() {
-	    	if(forward) {
-	    		position.x+=moveStep*Math.cos(Math.toRadians(rotation));
-	    		position.y+=moveStep*Math.sin(Math.toRadians(rotation));
-	    }
-	    	if(left) {
-	    		rotation-=5;
-	    	}
-	    	if(right) {
-	    		rotation+=5;
-	    	}
-	    	
-	    }
+		for (int i = 0; i < points.length; i++) {
+			x[i] = (int) points[i].getX();
+			y[i] = (int) points[i].getY();
+		}
 
-	    public void keyPressed(KeyEvent e) {
+		brush.drawPolygon(x, y, points.length);
+	}
 
-	        if(e.getKeyCode() == KeyEvent.VK_UP) {
-	            forward = true;
-	        }
+	public void move() {
+		if (forward) {
+			position.x += moveStep * Math.cos(Math.toRadians(rotation));
+			position.y += moveStep * Math.sin(Math.toRadians(rotation));
+		}
+		if (left) {
+			rotation -= 5;
+		}
+		if (right) {
+			rotation += 5;
+		}
+		if (position.x > 800) {
+			position.x = 0;
+		} else if (position.x < 0) {
+			position.x = 800;
+		}
 
-	        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-	            left = true;
-	        }
+		if (position.y > 600) {
+			position.y = 0;
+		} else if (position.y < 0) {
+			position.y = 600;
+		}
 
-	        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-	            right = true;
-	        }
-	    }
+	}
 
-	    public void keyReleased(KeyEvent e) {
+	public void keyPressed(KeyEvent e) {
 
-	        if(e.getKeyCode() == KeyEvent.VK_UP) {
-	            forward = false;
-	        }
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			forward = true;
+		}
 
-	        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-	            left = false;
-	        }
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = true;
+		}
 
-	        if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-	            right = false;
-	        }
-	    }
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = true;
+		}
+	}
 
-	    public void keyTyped(KeyEvent e) { }
+	public void keyReleased(KeyEvent e) {
+
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			forward = false;
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = false;
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = false;
+		}
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
 
 }
